b15cdf671fa42b70aee7e9fc6b7c3ee2bd683f00
czy77 <czy_git@163.com>
2021-05-26 08:29:22 +0800

    完成矩形绘制

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include "GLFW/glfw3.h"
 
 
-
-
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location =0) in vec3 aPos;\n"
                                  "void main()\n"
@@ -106,23 +104,31 @@ int main() {
     glDeleteShader(fragmentShader);
 
 
-
     float vertices[] = {
+            0.5f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
             -0.5f, -0.5, 0.0f,
-            0.5f, -0.5, 0.0f,
-            0.0f, 0.5f, 0.0f,
+            -0.5f, 0.5, 0.0f,
+    };
+
+    unsigned int indices[] = {
+            0, 1, 3,
+            1, 2, 3
     };
 
 
-    unsigned int VBO,VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     //绑定顶点数组,然后绑定顶点缓冲数组，最后配置顶点属性
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     //链接顶点属性
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
     //启用顶点属性
@@ -133,6 +139,8 @@ int main() {
     //顶点数组
     glBindVertexArray(0);
 
+    //使用线框模式绘制图形
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window)) {
         //输入
@@ -145,7 +153,9 @@ int main() {
         //使用上面创建的着色器程序
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
         //检查并调用事件,交换缓冲
         glfwSwapBuffers(window);
@@ -156,8 +166,8 @@ int main() {
     //释放资源
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
-
 
 
     glfwTerminate();
