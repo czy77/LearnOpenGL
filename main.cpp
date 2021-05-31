cc2e0f943bfe141233a846fb1ddea33da7ac4cfd
czy77 <czy_git@163.com>
2021-05-31 14:02:17 +0800

    更多的属性

--- main.cpp
+++ main.cpp
@@ -7,18 +7,21 @@
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "out vec3 ourColor;\n"
                                  "void main()\n"
                                  "{\n"
                                  "    gl_Position = vec4(aPos,1.0);\n"
+                                 "    ourColor = aColor;"
                                  "}\0";
 
 
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
-                                   "uniform vec4 ourColor;\n"
+                                   "in vec3 ourColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "    FragColor = ourColor;"
+                                   "    FragColor = vec4(ourColor,1.0);"
                                    "}\0";
 
 
@@ -108,9 +111,9 @@ int main() {
 
 
     float vertices[] = {
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5, 0.0f,
-            0.0f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
     };
 
     unsigned int VBO, VAO;
@@ -122,10 +125,13 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    //链接顶点属性
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
-    //启用顶点属性
+    //位置属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
+    //颜色属性
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
 
 //    //解绑缓冲数组
 //    glBindBuffer(GL_ARRAY_BUFFER, 0);
